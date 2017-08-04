3b262ad4147425e06363eb52dbf6b3c28360c0ce
Onur Guner <onur.guner@outlook.com>
2017-08-04 19:21:28 +0300

    add waypoint plugin

--- js/main.js
+++ js/main.js
@@ -25,29 +25,28 @@ $(document).ready(function() {
 	
 	/* Charts*/
 	$('.chart').easyPieChart({
-		barColor: '#3498db',
-		size: '120',
-		easing: 'easeOutBounce',
-		onStep: function(from, to, percent) {
-			$(this.el).find('.percent').text(Math.round(percent));
-		}
-	});
-	
-//	$('.chart').waypoint(function() {
-//			$(this).easyPieChart({
-//					barColor: '#3498db',
-//					size: '150',
-//					easing: 'easeOutBounce',
-//					onStep: function(from, to, percent) {
-//							$(this.el).find('.percent').text(Math.round(percent));
-//					}
-//			});
-//	}, {
-//			triggerOnce: true,
-//			offset: 'bottom-in-view'
+//	$('.chart').easyPieChart({
+//		barColor: '#3498db',
+//		size: '120',
+//		easing: 'easeOutBounce',
+//		onStep: function(from, to, percent) {
+//			$(this.el).find('.percent').text(Math.round(percent));
+//		}
 //	});
 	
-
+	$('.chart').waypoint(function() {
+		$('.chart').easyPieChart({
+			barColor: '#3498db',
+			size: '120',
+			easing: 'easeOutBounce',
+			onStep: function(from, to, percent) {
+				$(this.el).find('.percent').text(Math.round(percent));
+			}
+		});
+	}, {
+		offset: 'bottom-in-view'
+	});
+	
 	/* VEGAS Home Slider */
 	$('#page-welcome').vegas({
 		slides: [
