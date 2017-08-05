e387d9bf7f4dbdda0500b373518b9fb74f04f0e8
Onur Guner <onur.guner@outlook.com>
2017-08-05 18:10:42 +0300

    add cache to ajax

--- js/contact.me.js
+++ js/contact.me.js
@@ -21,7 +21,6 @@ $(function() {
           method: "POST",
           data: {name: name, email: email, message: message},
 					dataType: "json",
-          cache: false,
           success: function() {  
           	// Success message
           	$('#success').html("<div class='alert alert-success'>");
